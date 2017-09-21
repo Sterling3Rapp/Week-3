5e584317d0b1c02a749e7859e65fe59f15a408ca
Sterling Rapposelli III <rapp7889@chawk.cecil.edu>
2017-09-21 09:54:18 -0400

    This is what we did in Week 3

--- street address.py
+++ street address.py
@@ -1,8 +1,15 @@
+import requests
+import bs4
 import re
 
-s = "1 Seahawk Dr 123 Main Street 20 Sycamore Ln "
+r = requests.get('http://classifieds.delawareonline.com/map.php?place=Garage+Sales&posit=Garage+Sales')
+soup = bs4.BeautifulSoup(r.content,'html.parser')
+s = str(soup)
+street = "20 Sycamore Ln 123 Main St 5987 Carriage Ct"
 
-address = re.compile(r'\d+ \w {2}')
+address = re.compile(r'\d+\s\w+\s\w{2}')
+result = address.findall(street)
+yardsales = address.findall(s)
 
-result = address.findall(s)
-print = result
+print(result)
+print(yardsales)
